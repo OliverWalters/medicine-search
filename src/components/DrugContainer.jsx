0f44da472b30b18283d0735dcc428c7b1dc218b3
Oliver <114961404+OliverWalters@users.noreply.github.com>
2024-06-21 20:39:01 +0200

    More updates

--- src/components/DrugContainer.jsx
+++ src/components/DrugContainer.jsx
@@ -1,9 +1,22 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, TextField, Box } from "@mui/material";
 import VaccinesIcon from '@mui/icons-material/Vaccines';
+import { LoadingButton } from '@mui/lab';
+import { useDrugSearch } from '../services/useDrugSearch';
 import { DrugList } from "./DrugList";
 import "../assets/DrugContainer.css";
 
 export function DrugContainer() {
+    const {
+        data,
+        drug,
+        loading,
+        error,
+        end,
+        setDrug,
+        onSubmit,
+        loadMore
+    } = useDrugSearch();
+
     return (
         <Container className="container"
             maxWidth="lg"
@@ -18,8 +31,55 @@ export function DrugContainer() {
                 gutterBottom
             ><VaccinesIcon fontSize="20" sx={{ mr: 2 }} />Drugs Search</Typography>
 
-            
-            <DrugList />
+
+            <Box className="drugList"
+                component="form"
+                autoComplete="off"
+                onSubmit={onSubmit}
+            >
+                <TextField className="drugList__input"
+                    id="drug"
+                    label="Drug"
+                    variant="outlined"
+                    size="small"
+                    value={drug}
+                    onChange={(e) => { setDrug(e.target.value); }}
+                    error={error.error}
+                    helperText={error.message}
+                />
+
+                <LoadingButton
+                    className="drugList__btn"
+                    type="submit"
+                    variant="contained"
+                    loading={loading}
+                    loadingIndicator="Loading..."
+                >
+                    Search
+                </LoadingButton>
+
+                <Box className="drugList__results">
+                    {data && data.results && data.results.map((drug) => (
+                        <DrugList drug={drug} />
+                    ))}
+                </Box>
+
+                {data && data.meta.results.total > 10 && data.meta.results.total > end && (
+                    <LoadingButton
+                        onClick={loadMore}
+                        variant="contained"
+                        loading={loading}
+                        loadingIndicator="Loading..."
+                        sx={{ width: 400 }}
+                    >
+                        See more
+                    </LoadingButton>
+                )}
+            </Box>
+
+
+
+
         </Container>
 
     );
--- src/components/DrugList.jsx
+++ src/components/DrugList.jsx
@@ -1,75 +1,21 @@
-import { Box, TextField, Typography } from "@mui/material";
-import { LoadingButton } from '@mui/lab';
+import { Box, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { useDrugSearch } from '../services/useDrugSearch';
 
-export function DrugList() {
-    const {
-        data,
-        drug,
-        loading,
-        error,
-        end,
-        setDrug,
-        onSubmit,
-        loadMore
-    } = useDrugSearch();
+
+export function DrugList({ drug }) {
 
     const navigate = useNavigate();
 
     return (
-        <Box className="drugList"
-            component="form"
-            autoComplete="off"
-            onSubmit={onSubmit}
+        <Box
+            key={drug.product_id}
+            id={drug.product_id}
+            onClick={() => navigate(`/drug/${drug.product_id}`)}
+            sx={{ width: 1000, border: 1, borderColor: "grey.500", borderRadius: 1, p: 2, m: 1, cursor: "pointer" }}
         >
-            <TextField className="drugList__input"
-                id="drug"
-                label="Drug"
-                variant="outlined"
-                size="small"
-                value={drug}
-                onChange={(e) => { setDrug(e.target.value); }}
-                error={error.error}
-                helperText={error.message}
-            />
-
-            <LoadingButton
-                className="drugList__btn"
-                type="submit"
-                variant="contained"
-                loading={loading}
-                loadingIndicator="Loading..."
-            >
-                Search
-            </LoadingButton>
-
-            <Box className="drugList__results">
-                {data && data.results && data.results.map((drug) => (
-                    <Box
-                        key={drug.product_id}
-                        id={drug.product_id}
-                        onClick={() => navigate(`/drug/${drug.product_id}`)}
-                        sx={{ width: 1000, border: 1, borderColor: "grey.500", borderRadius: 1, p: 2, m: 1, cursor: "pointer" }}
-                    >
-                        <Typography fontWeight="bold">{drug.generic_name}</Typography>
-                        <Typography>Brand: {drug.brand_name}</Typography>
-                        <Typography>Company: {drug.labeler_name}</Typography>
-                    </Box>
-                ))}
-            </Box>
-
-            {data && data.meta.results.total > 10 && data.meta.results.total > end && (
-                <LoadingButton
-                    onClick={loadMore}
-                    variant="contained"
-                    loading={loading}
-                    loadingIndicator="Loading..."
-                    sx={{ width: 400 }}
-                >
-                    See more
-                </LoadingButton>
-            )}
+            <Typography fontWeight="bold">{drug.generic_name}</Typography>
+            <Typography>Brand: {drug.brand_name}</Typography>
+            <Typography>Company: {drug.labeler_name}</Typography>
         </Box>
     );
 }
